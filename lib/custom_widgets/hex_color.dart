7915e02468ea3be950bf2f2ce87d2aaca91aa60f
magdaramadan188 <magdaramadan188@gmail.com>
2025-08-28 20:29:54 +0300

    Delete lib directory

--- lib/custom_widgets/hex_color.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-HexColor(String hexColorString) {
-  final hexCode = hexColorString.replaceAll('#', '');
-  return Color(int.parse('FF$hexCode', radix: 16));
-}
